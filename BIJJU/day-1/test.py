9d777025d7efb3542a28fe161bcfa8c84f0be71c
Brejesh-J <brejeshj@gmail.com>
2026-09-24 20:49:12 +0530

    day 1

---
+++ BIJJU/day-1/test.py
@@ -0,0 +1,11 @@
+num1= float(input("enter the first number"))
+num2= float(input("enter the second number"))
+sum_result = num1 + num2
+print (f"sum: {num1} + {num2} = {sum_result}")
+num3= float(input("enter the third number"))
+num4= float(input("enter the fourth number"))
+if num4==0:
+    print("zero is not allowed")
+else:
+    div_result= num3/num4
+    print(f"division: {num3} / {num4} = {div_result}")
